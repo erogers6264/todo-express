4c110a0ce054b33ef6bea18fce89a842f409dfc5
Ethan Rogers <erogers6264@gmail.com>
2018-03-28 01:50:51 +0000

    Define a delete route

--- routes/todos.js
+++ routes/todos.js
@@ -46,4 +46,15 @@ router.put('/:todoId', function(req, res){
     });
 });
 
+// Delete a todo
+router.delete('/:todoId', function(req, res){
+    db.Todo.remove({_id: req.params.todoId})
+    .then(function(){
+        res.json({message: 'Todo has been deleted'});
+    })
+    .catch(function(err){
+        res.send(err);
+    });
+});
+
 module.exports = router;
